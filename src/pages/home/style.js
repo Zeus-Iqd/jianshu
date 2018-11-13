9bf58f2b2be8e992e0793b580c43b17256ce8485
周帅 <zhoushuai@MacBook-Pro.local>
2018-11-13 23:28:18 +0800

    增加路由

---
+++ src/pages/home/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const HomeWrapper = styled.div`
+  width: 960px;
+  height: 300px;
+  overflow:hidden;
+  margin: 0 auto;
+`
+export const HomeLeft = styled.div`
+  float: left;
+  margin-left: 15px;
+  padding-top: 30px;
+  width: 625px;
+`
+export const HomeRight = styled.div`
+  width: 240px;
+  float: left;
+`
